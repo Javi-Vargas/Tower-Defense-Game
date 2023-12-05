8acb88757f82d372a82757d1be414730f7945755
Javi-Vargas <105886463+Javi-Vargas@users.noreply.github.com>
2023-12-05 16:39:06 -0500

    Create enemy obj.
    Create enemy obj and constructor. All its private variables and getters as well. Wrote the move() that commands enemies to move.

--- TDGame/src/enemies/Enemy.java
+++ TDGame/src/enemies/Enemy.java
@@ -1,1 +1,53 @@
+package enemies;
 
+import java.awt.Rectangle;
+
+public class Enemy {
+
+	private float x, y;
+	private Rectangle bounds;
+	private int health;
+	private int ID;
+	private int enemyType;
+	
+	public Enemy(float x, float y, int id, int enemyType)
+	{
+		this.x=x;
+		this.y =y;
+		this.ID = ID;
+		this.enemyType = enemyType;
+		bounds = new Rectangle((int)x, (int)y, 32,32);
+	}
+	
+	public void move(float x, float y)
+	{
+		this.x+=x;
+		this.y+=y;
+	}
+
+	public float getX() {
+		return x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public Rectangle getBounds() {
+		return bounds;
+	}
+
+	public int getHealth() {
+		return health;
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public int getEnemyType() {
+		return enemyType;
+	}
+	
+	
+}
