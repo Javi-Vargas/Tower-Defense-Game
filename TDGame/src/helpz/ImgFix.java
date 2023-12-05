ed9ea637c822cf1b5cb01ebf951bf2c1eedf5744
Javi-Vargas <105886463+Javi-Vargas@users.noreply.github.com>
2023-12-05 12:46:29 -0500

    Updated methods to create animation to sprites.
    Editing getBuildRotImg to take in BufferedImage Arraylist. Rotating the water in the sprite and assigning it to new img. That will be then added to the list, and return list.

--- TDGame/src/helpz/ImgFix.java
+++ TDGame/src/helpz/ImgFix.java
@@ -63,4 +63,30 @@ public class ImgFix {
 		return newImg;
 	}
 	
+	
+	//rotate second image only + animation
+	public static BufferedImage[] getBuildRotImg(BufferedImage[] imgs, BufferedImage secondImage, int rotAngle)
+	{
+		int w = imgs[0].getWidth();
+		int h = imgs[0].getHeight();
+		BufferedImage[] arr = new BufferedImage[imgs.length];
+		
+		for(int i=0; i<imgs.length;i++)
+		{
+			BufferedImage newImg = new BufferedImage(w, h, imgs[0].getType());
+			Graphics2D g2d = newImg.createGraphics();
+			
+			g2d.drawImage(imgs[i], 0, 0, null);
+			g2d.rotate(Math.toRadians(rotAngle), w/2, h/2);
+			g2d.drawImage(secondImage, 0, 0, null);
+
+			g2d.dispose();
+			
+			arr[i] = newImg;
+		}
+		
+
+		return arr;
+	}
+	
 }
